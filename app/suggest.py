8698cf75a4a89e21c4fec0c62eb597d3c2654401
Alex Knowles <ark@looker.com>
2015-02-28 01:00:31 -0800

    support for google.com/s (used in maps)

--- app/suggest.py
+++ app/suggest.py
@@ -43,19 +43,26 @@ class SuggestXmlHandler(wtwfhandler.WtwfHandler):
     self.response.headers['Content-Type'] = xmltype
     self.SendTemplate('opensearch.xml', template_values)
 
-JSONP_START_RE = re.compile(r'^[a-zA-Z0-9_.]+\(', re.MULTILINE)
-FIXUP_MAP = {}
+def fixupGoogles(keyword, reply_key, reply):
+  """the google.com/s format."""
+  obj = json.loads(getJson(reply))
+  # the first element of arrays that are 4 deep
+  choices = flatten(firstNdeep(obj, 4))
+  choices = ['%s %s' % (keyword, x) for x in choices if isinstance(x, (str, unicode)) and x[-1] != '=']
+  return json.dumps([reply_key, choices])
 
-def getUrlHost(url):
-  try:
-    return url[0:url.find('/', 8)]
-  except e:
-    pass
-  return url
+
+JSONP_START_RE = re.compile(r'^[a-zA-Z0-9_.]+\(', re.MULTILINE)
+FIXUP_MAP = {
+  r'^https://www\.google\.com/s\?tbm=': fixupGoogles
+}
 
 def getJson(s):
   try:
-    return s[s.find('['):s.rfind(']') + 1]
+    s = s[s.find('['):s.rfind(']') + 1]
+    if s.find('[\\"') != -1:
+      s = s.decode('string_escape') # google.com/s does this. Sends back a json string as a key to a dict.
+    return s
   except e:
     pass
   return s
@@ -66,10 +73,9 @@ def fixupSuggestReply(url, parts, reply):
   search_term = parts[1]
   reply_key = ' '.join(parts)
 
-  # get domain of url
-  url = getUrlHost(url)
-  if url in FIXUP_MAP:
-    return FIXUP_MAP[url](keyword, reply)
+  for (regex, func) in FIXUP_MAP.items():
+    if re.search(regex, url):
+      return func(keyword, reply_key, reply)
 
   # make sure it's not jsonp
   reply = getJson(reply)
@@ -83,11 +89,18 @@ def fixupSuggestReply(url, parts, reply):
     obj[0] = reply_key
     obj[1] = [fixupSuggestReplyEntry(keyword, x) for x in obj[1]]
   else:
+    obj = obj[0]
+    # logging.info('%r', obj)
+    for x in obj:
+      logging.info('%r', x)
+
     # assume result is ['option1', 'option2']
     obj = [reply_key, [fixupSuggestReplyEntry(keyword, x) for x in obj]]
 
   # then reencode it and send it back
-  return json.dumps(obj)
+  ans = json.dumps(obj)
+  logging.info('%r', ans)
+  return ans
 
 def fixupSuggestReplyEntry(keyword, entry):
   if isinstance(entry, basestring):
@@ -99,3 +112,20 @@ BOLD_RE = re.compile(r'(\u003C/?b\u003E|</?b>)', re.MULTILINE)
 
 def removeBold(entry):
   return BOLD_RE.sub('', entry)
+
+def flatten(i):
+  if isinstance(i, list) or isinstance(i, tuple):
+    for j in i:
+      for x in flatten(j):
+        yield x
+  else:
+    if i:
+      yield i
+
+def firstNdeep(arr, depth, currentDepth=0):
+  if not isinstance(arr, list):
+    return None
+
+  if depth == currentDepth:
+    return arr[0]
+  return [firstNdeep(x, depth, currentDepth + 1) for x in arr]
